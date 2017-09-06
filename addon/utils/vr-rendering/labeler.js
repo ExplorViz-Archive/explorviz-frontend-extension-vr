3a55eae73cf61d1fd4f6c2e6b582cd8857fc1994
Timm Häsemeyer <stu113826@mail.uni-kiel.de>
2017-09-06 12:06:18 +0200

    Update labeler.js

--- addon/utils/vr-rendering/labeler.js
+++ addon/utils/vr-rendering/labeler.js
@@ -32,6 +32,7 @@ export default Ember.Object.extend({
     }
 
     this.get(textCache).push({text: text, parent: parent, color: color, boxColor: boxColor});
+
   },
 
 
@@ -53,15 +54,34 @@ export default Ember.Object.extend({
 
   },
 
+  /*
+  * This method is used to mark given entities
+  *
+  */
   redrawLabel(entity, textColor, name, color){
 
-    // Create one canvas for each entity
+    // Create one canvas for each entity if not already exists
     if(!this.get('canvasList')[entity.id]){
       let canvas = document.createElement('canvas');
       canvas.width = 64;
       canvas.height = 32;
+
       this.get('canvasList')[entity.id] = canvas;
+
+      entity.geometry.computeBoundingBox();
+      let bbox = entity.geometry.boundingBox;
+      
+      let size = bbox.getSize();
+      
+      // calculate aspect ratio and next power of 2 
+      let nextPowerOf2X = Math.pow(2, Math.ceil(Math.log(size.x*40)/Math.log(2)));
+      let nextPowerOf2Y = Math.pow(2, Math.ceil(Math.log(size.y*40)/Math.log(2)));
+
+      // Adapt canvas to size of box
+      this.get('canvasList')[entity.id].width = nextPowerOf2X;
+      this.get('canvasList')[entity.id].height = nextPowerOf2Y;
     }
+
 
     let oldMaterial = new THREE.MeshBasicMaterial({color});
 
@@ -69,24 +89,47 @@ export default Ember.Object.extend({
     
     var ctx = canvas.getContext('2d');
 
+    var x, y, max;
+
+    if(entity.type === 'system'){
+      if(entity.userData.model.get('opened')){
+        ctx.font = '15px arial';
+        y = canvas.height/16;
+       
+      }
+      else{
+        ctx.font = '32px arial';
+        y = canvas.height/2;
+      }
+      x = canvas.width/2;
+      max = canvas.width*0.95;
+    
+    }
+    else if(entity.type === 'nodegroup'){
+      if(!entity.userData.model.get('opened')){
+        ctx.font = '30px arial';
+        x = canvas.width/2;
+        y = canvas.height/2;
+        max = canvas.width*0.95;
+      } 
+ 
+    }
+    else {
+      ctx.font = '30px arial';
+      x = canvas.width/2*0.8;
+      y = canvas.height/2;
+      max = canvas.width*0.5;
+
+    }
+
     // Draw old box color
     ctx.fillStyle = color;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = textColor;
-    // Handle opened and closed systems
-    if(entity.userData.model.get('opened')){
-      // Draw title for opened systems
-      ctx.font = '15px arial';
-      ctx.textAlign = "center";
-      ctx.fillText(name, canvas.width/2,canvas.height/10,canvas.width-4);
-    }
-    else{
-      // Draw title for closed systems
-      ctx.font = '32px arial';
-      ctx.textAlign = "center";
-      ctx.fillText(name, canvas.width/2,canvas.height/2,canvas.width-4);
-    }
- 
+    
+    ctx.textAlign = "center";
+    ctx.fillText(name, x,y,max);
+    
     // create texture out of canvas
     let texture = new THREE.Texture(canvas);
 
@@ -153,14 +196,14 @@ export default Ember.Object.extend({
         ctx.font = '20px arial';
         ctx.fillStyle = textObj.color;
         ctx.textAlign = "center";
-        ctx.fillText(textObj.text, canvas.width/2,canvas.height/10,canvas.width-4);
+        ctx.fillText(textObj.text, canvas.width/2,canvas.height/16,canvas.width*0.95);
       }
       else{
         // Draw title for closed systems
-        ctx.font = '40px arial';
+        ctx.font = '32px arial';
         ctx.fillStyle = textObj.color;
         ctx.textAlign = "center";
-        ctx.fillText(textObj.text, canvas.width/2,canvas.height/2,canvas.width-4);
+        ctx.fillText(textObj.text, canvas.width/2,canvas.height/2,canvas.width*0.95);
       }
 
       // create texture out of canvas
@@ -230,7 +273,7 @@ export default Ember.Object.extend({
         ctx.font = '30px arial';
         ctx.fillStyle = textObj.color;
         ctx.textAlign = "center";
-        ctx.fillText(textObj.text, canvas.width/2,canvas.height/2,canvas.width-4);
+        ctx.fillText(textObj.text, canvas.width/2,canvas.height/2,canvas.width*0.95);
       }
 
       // create texture out of canvas
