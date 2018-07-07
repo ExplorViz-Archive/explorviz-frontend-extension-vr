a0071f4f1bcf84d64994151a59613e442d451880
mha <stu126317@mail.uni-kiel.de>
2018-07-07 17:29:58 +0200

    deleted unneccessary comments/functions

--- addon/components/web-socket.js
+++ addon/components/web-socket.js
@@ -3,28 +3,17 @@ import { inject as service } from '@ember/service';
 
 export default Component.extend({
 
-  /*
-   * 1. Inject the websockets service
-   */
+  //inject service
   websockets: service(),
   socketRef: null,
 
   didInsertElement() {
     this._super(...arguments);
 
-    /*
-      2. The next step you need to do is to create your actual websocket. Calling socketFor
-      will retrieve a cached websocket if one exists or in this case it
-      will create a new one for us.
-    */
+    //create WebSocket
     const socket = this.websockets.socketFor('ws://localhost:4444/');
 
-    /*
-      3. The next step is to define your event handlers. All event handlers
-      are added via the `on` method and take 3 arguments: event name, callback
-      function, and the context in which to invoke the callback. All 3 arguments
-      are required.
-    */
+    //event handlers
     socket.on('open', this.myOpenHandler, this);
     socket.on('message', this.myMessageHandler, this);
     socket.on('close', this.myCloseHandler, this);
@@ -37,9 +26,7 @@ export default Component.extend({
 
     const socket = this.socketRef;
 
-    /*
-      4. The final step is to remove all of the listeners you have setup.
-    */
+    //remove listeners
     socket.off('open', this.myOpenHandler);
     socket.off('message', this.myMessageHandler);
     socket.off('close', this.myCloseHandler);
@@ -55,11 +42,6 @@ export default Component.extend({
 
   myCloseHandler(event) {
     console.log(`On close event has been called: ${event}`);
-  },
-
-  actions: {
-    sendButtonPressed() {
-      this.socketRef.send('Hello Websocket World');
-    }
   }
+  
 });
