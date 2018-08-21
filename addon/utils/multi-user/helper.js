8073af7d53e3bf36e9d6ebd7203124c8c403a2b3
mha <stu126317@mail.uni-kiel.de>
2018-08-21 15:23:48 +0200

    more modular code

---
+++ addon/utils/multi-user/helper.js
@@ -0,0 +1,51 @@
+  /**
+   * 
+   * @param {string} text The text to measure the width, height and subline height of.
+   * @param {string} font The font to measure the size in.
+   * @return {{width: Number, height: Number, sublineHeight: Number}} The sizes of the text.
+   */
+  export function getTextSize(text, font) {
+    // re-use canvas object for better performance
+    let canvas = document.createElement("canvas");
+    let context = canvas.getContext("2d");
+    context.font = font;
+    let width = context.measureText(text).width;
+    let height = context.measureText("W").width;
+    var sublineHeight = context.measureText("H").width;
+    return { width, height, sublineHeight };
+  }
+
+  export function rgbToHex(rgbArray) {
+    return "#" + ((1 << 24) + (rgbArray[0] << 16) + (rgbArray[1] << 8) + rgbArray[2]).toString(16).slice(1);
+  }
+
+      /**
+   * Performs a binary search on the given array.
+   * 
+   * @param {Array} array The array to search through.
+   * @param {*} searchElement The item to search for within the array.
+   * @return {Number} The index of the element which defaults to -1 when not found.
+   */
+  export function binaryIndexOf(array, searchElement) {
+    var minIndex = 0;
+    var maxIndex = array.length - 1;
+    var currentIndex;
+    var currentElement;
+
+    while (minIndex <= maxIndex) {
+        currentIndex = (minIndex + maxIndex) / 2 | 0;
+        currentElement = array[currentIndex];
+
+        if (currentElement < searchElement) {
+            minIndex = currentIndex + 1;
+        }
+        else if (currentElement > searchElement) {
+            maxIndex = currentIndex - 1;
+        }
+        else {
+            return currentIndex;
+        }
+    }
+
+    return -1;
+  }
