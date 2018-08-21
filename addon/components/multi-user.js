f63b112bcb223892f44826d0ede6d1bded9efe86
mha <stu126317@mail.uni-kiel.de>
2018-08-21 15:23:03 +0200

    Functions for sending messages and general helper functions are in their own files now (Refactoring)

--- addon/components/multi-user.js
+++ addon/components/multi-user.js
@@ -1,6 +1,8 @@
 import { inject as service } from '@ember/service';
 import User from '../utils/multi-user/user';
 import Menu from '../utils/multi-user/menu';
+import Helper from '../utils/multi-user/helper';
+import Sender from '../utils/multi-user/send';
 import VRRendering from './vr-rendering';
 import Ember from 'ember';
 import THREE from 'three';
@@ -26,7 +28,7 @@ export default VRRendering.extend(Ember.Evented, {
   state: null, // own connection status, state in {'connecting', 'connected', 'spectating'}
   lastPositions: null, // last positions of camera and controllers
   controllersConnected: null, // tells which controller(s) are connected
-  fps: 90, // tells how often a picture is rendered per second (refresh rate of Vive/Rift is 90)
+  fps: 90, // tells how many pictures are max. rendered per second (refresh rate of Vive/Rift is 90)
   lastTime: null, // last time an image was rendered
   currentTime: 0, // tells the current time in ms
   deltaTime: 0, // time between two frames
@@ -39,7 +41,7 @@ export default VRRendering.extend(Ember.Evented, {
   optionsMenu: null, // current options menu, null if all closed
   userListMenu: null, // keep track of the user list menu
   startPosition: null, //position before this user starts spectating
-  session: Ember.inject.service('session'),
+  session: Ember.inject.service('session'), //session used to retrieve username
 
 
   gameLoop() {
@@ -124,7 +126,7 @@ export default VRRendering.extend(Ember.Evented, {
     spectatedUser.camera.model.visible = false;
     spectatedUser.namePlane.visible = false;
     this.set('state', 'spectating');
-    this.sendSpectatingUpdate();
+    Sender.sendSpectatingUpdate.call(this);
   },
 
   deactivateSpectating(){
@@ -144,7 +146,7 @@ export default VRRendering.extend(Ember.Evented, {
     let position = this.get('startPosition');
     this.get('user.position').fromArray(position.toArray());
 
-    this.sendSpectatingUpdate();
+    Sender.sendSpectatingUpdate.call(this);
   },
 
 
@@ -267,31 +269,31 @@ export default VRRendering.extend(Ember.Evented, {
 
     //initialize interaction events and delegate them to the corresponding functions
     this.get('interaction').on('systemStateChanged', (id, isOpen) => {
-      this.sendSystemUpdate(id, isOpen);
+      Sender.sendSystemUpdate.call(this, id, isOpen);
     });
     this.get('interaction').on('nodegroupStateChanged', (id, isOpen) => {
-      this.sendNodegroupUpdate(id, isOpen);
+      Sender.sendNodegroupUpdate.call(this, id, isOpen);
     });
     this.on('applicationOpened', (id, app) => {
-      this.sendAppOpened(id, app);
+      Sender.sendAppOpened.call(this, id, app);
     });
     this.get('interaction').on('removeApplication',(appID) => {
-      this.sendAppClosed(appID);
+      Sender.sendAppClosed.call(this, appID);
     });
     this.get('interaction').on('appReleased',(appID, position, quaternion) => {
-      this.sendAppReleased(appID, position, quaternion);
+      Sender.sendAppReleased.call(this, appID, position, quaternion);
     });
     this.get('interaction').on('appBinded',(appID, appPosition, appQuaternion, isBoundToController1, controllerPosition, controllerQuaternion) => {
-      this.sendAppBinded(appID, appPosition, appQuaternion, isBoundToController1, controllerPosition, controllerQuaternion);
+      Sender.sendAppBinded.call(this, appID, appPosition, appQuaternion, isBoundToController1, controllerPosition, controllerQuaternion);
     });
     this.get('interaction').on('componentUpdate', (appID , componentID, isOpened) => {
-      this.sendComponentUpdate(appID, componentID, isOpened);
+      Sender.sendComponentUpdate.call(this, appID, componentID, isOpened);
     });
     this.get('interaction').on('landscapeMoved', (deltaPosition) => {
-      this.sendLandscapeUpdate(deltaPosition);
+      Sender.sendLandscapeUpdate.call(this, deltaPosition);
     });
     this.get('interaction').on('entityHighlighted', (isHighlighted, appID, entityID, color) => {
-      this.sendHighlightingUpdate(isHighlighted, appID, entityID, color);
+      Sender.sendHighlightingUpdate.call(this, isHighlighted, appID, entityID, color);
     });
   },
 
@@ -595,7 +597,7 @@ export default VRRendering.extend(Ember.Evented, {
               return;
             }
             
-            let index = this.binaryIndexOf(userArray, this.spectatedUser);
+            let index = Helper.binaryIndexOf(userArray, this.spectatedUser);
 
             if(index !== -1) {
               if(index === userArray.length - 1) {
@@ -628,7 +630,7 @@ export default VRRendering.extend(Ember.Evented, {
                 return;
               }
             
-              let index = this.binaryIndexOf(userArray, this.spectatedUser);
+              let index = Helper.binaryIndexOf(userArray, this.spectatedUser);
   
               if(index !== -1) {
                 if(index === 0) {
@@ -763,7 +765,7 @@ export default VRRendering.extend(Ember.Evented, {
 
     for(let i = 0; i < playingUsers.length; i++) {
       let userColor = playingUsers[i].color;
-      menu.addText(playingUsers[i].name, 'connected', 12, { x: 50, y: yPos + i*yOffset}, this.rgbToHex(userColor), 'left', false);
+      menu.addText(playingUsers[i].name, 'connected', 12, { x: 50, y: yPos + i*yOffset}, Helper.rgbToHex(userColor), 'left', false);
     }
 
     yPos = yPos + yOffset*(playingUsers.length);
@@ -779,7 +781,7 @@ export default VRRendering.extend(Ember.Evented, {
     
     for(let i = 0; i < spectatingUsers.length; i++) {
       let userColor = spectatingUsers[i].color;
-      menu.addText(spectatingUsers[i].name, 'spectating', 12, { x: 50, y: yPos + i*yOffset}, this.rgbToHex(userColor), 'left', false);
+      menu.addText(spectatingUsers[i].name, 'spectating', 12, { x: 50, y: yPos + i*yOffset}, Helper.rgbToHex(userColor), 'left', false);
     }
 
     menu.interact = (action, position) => {};
@@ -815,240 +817,7 @@ export default VRRendering.extend(Ember.Evented, {
    */
   update() {
     this.updateAndSendPositions();
-    this.sendControllerUpdate();
-  },
-
-  /**
-   * Send update of position + quaternion of the
-   * landscape (vrEnvironment)
-   */
-  sendLandscapeUpdate(deltaPosition){
-    let quaternion =  this.get('vrEnvironment').quaternion;
-
-    let landscapeObj = {
-      "event": "receive_landscape_position",
-      "time": Date.now(),
-      "deltaPosition" : deltaPosition.toArray(),
-      "offset" : this.get('environmentOffset').toArray(),
-      "quaternion" : quaternion.toArray()
-    }
-    this.updateQueue.push(landscapeObj);
-  },
-
-  /**
-   * Send the backend the information that a system was
-   * closed or opened by this user
-   * @param {Long} id ID of system which was opened/closed
-   * @param {boolean} isOpen State of the system
-   */
-  sendSystemUpdate(id, isOpen){
-    console.log("Sending system update");
-    let systemObj = {
-      "event": "receive_system_update",
-      "time": Date.now(),
-      "id": id,
-      "isOpen": isOpen
-    }
-    this.updateQueue.push(systemObj);
-  },
-
-  /**
-   * Send the backend the information that a nodegroup was
-   * closed or opened by this user
-   * @param {Long} id ID of nodegroup which was opened/closed
-   * @param {boolean} isOpen State of the nodegroup
-   */
-  sendNodegroupUpdate(id, isOpen){
-    let nodeGroupObj = {
-      "event": "receive_nodegroup_update",
-      "time": Date.now(),
-      "id": id,
-      "isOpen": isOpen
-    }
-    this.updateQueue.push(nodeGroupObj);
-  },
-
-  /**
-   * Inform the backend that an app was opened by this
-   * user
-   * @param {Long} id ID of nodegroup which was opened/closed
-   * @param {boolean} isOpen State of the nodegroup
-   */
-  sendAppOpened(id, app){
-    let position = new THREE.Vector3();
-    app.getWorldPosition(position);
-    
-    let quaternion = new THREE.Quaternion();
-    app.getWorldQuaternion(quaternion);
-
-    let appObj = {
-      "event": "receive_app_opened",
-      "time": Date.now(),
-      "id": id,
-      "position" : position.toArray(),
-      "quaternion" : quaternion.toArray()
-    }
-    this.updateQueue.push(appObj);
-  },
-
-  /**
-   * Inform the backend that an application was closed
-   * by this user
-   * @param {Long} appID ID of the closed application
-   */
-  sendAppClosed(appID){
-    let appObj = {
-      "event": "receive_app_closed",
-      "time": Date.now(),
-      "id": appID
-    }
-    this.updateQueue.push(appObj);
-  },
-
-  /**
-   * Informs the backend that this user holds/moves an application
-   * @param {Long} appID ID of the bound app
-   * @param {Vector3} appPosition Position of the app (x, y, z)
-   * @param {Quaternion} appQuaternion Quaternion of the app (x, y, z, w)
-   * @param {boolean} isBoundToController1 Tells if app is hold by left controller
-   * @param {Vector3} controllerPosition Position of the controller which holds the application
-   * @param {Quaternion} controllerQuaternion Quaternion of the controller which holds the application
-   */
-  sendAppBinded(appID, appPosition, appQuaternion, isBoundToController1, controllerPosition, controllerQuaternion){
-    let appObj = {
-      "event": "receive_app_binded",
-      "time": Date.now(),
-      "appID": appID,
-      "appPosition" : appPosition.toArray(),
-      "appQuaternion" : appQuaternion.toArray(),
-      "isBoundToController1" : isBoundToController1,
-      "controllerPosition" : controllerPosition.toArray(),
-      "controllerQuaternion" : controllerQuaternion.toArray()
-    }
-    this.updateQueue.push(appObj);
-  },
-
-  /**
-   * Informs the backend that an application is no longer bound but released
-   * @param {Long} appID ID of the bound app
-   * @param {Vector3} position Position of the app (x, y, z)
-   * @param {Quaternion} quaternion Quaternion of the app (x, y, z, w)
-   */
-  sendAppReleased(appID, position, quaternion){
-    let appObj = {
-      "event": "receive_app_released",
-      "time": Date.now(),
-      "id": appID,
-      "position" : position.toArray(),
-      "quaternion" : quaternion.toArray()
-    }
-    this.updateQueue.push(appObj);
-  },
-
-  /**
-   * Informs the backend that a component was opened or closed by this user
-   * @param {Long} appID ID of the app which is a parent to the component
-   * @param {Long} componentID ID of the component which was opened or closed
-   * @param {boolean} isOpened Tells whether the component is now open or closed (current state)
-   */
-  sendComponentUpdate(appID, componentID, isOpened){
-    let appObj = {
-      "event": "receive_component_update",
-      "time": Date.now(),
-      "appID": appID,
-      "componentID": componentID,
-      "isOpened": isOpened
-    }
-    this.updateQueue.push(appObj);
-  },
-
-  /**
-   * Informs the backend that an entity (clazz or component) was highlighted
-   * or unhighlighted
-   * @param {boolean} isHighlighted Tells whether the entity has been highlighted or not
-   * @param {Long} appID ID of the parent application of the entity
-   * @param {Long} entityID ID of the highlighted/unhighlighted component/clazz
-   * @param {Color} color Original color of the entity as hex value
-   */
-  sendHighlightingUpdate(isHighlighted, appID, entityID, color){
-    let hightlightObj = {
-      "event": "receive_hightlight_update",
-      "time": Date.now(),
-      "userID" : this.get('userID'),
-      "appID": appID,
-      "entityID": entityID,
-      "isHighlighted": isHighlighted,
-      "color": color
-    }
-    this.updateQueue.push(hightlightObj);
-  },
-
-  sendSpectatingUpdate(){
-    let spectateObj = {
-      "event": "receive_spectating_update",
-      "userID": this.get('userID'),
-      "isSpectating": this.state === 'spectating',
-      "spectatedUser": this.get('spectatedUser'),
-      "time": Date.now()
-    }
-    this.updateQueue.push(spectateObj);
-  },
-
-  /**
-   * Informs the backend if a controller was connected/disconnected
-   */
-  sendControllerUpdate() {
-    let controllerObj = {
-      "event": "receive_user_controllers",
-      "time": Date.now()
-    };
-
-    let disconnect = [];
-    let connect = {};
-
-    let hasChanged = false;
-
-    //handle that controller 1 has disconnected
-    if(this.controllersConnected.controller1 && this.controller1.getGamepad() === undefined) {
-      disconnect.push("controller1");
-      this.controllersConnected.controller1 = false;
-      hasChanged = true;
-    }
-    //handle that controller 1 has connected
-    else if(!this.controllersConnected.controller1 && this.controller1.getGamepad() !== undefined) {
-      connect.controller1 = this.controller1.getGamepad().id;
-      this.controllersConnected.controller1 = true;
-      hasChanged = true;
-    }
-
-    //handle that controller 2 has disconnected
-    if(this.controllersConnected.controller2 && this.controller2.getGamepad() === undefined) {
-      disconnect.push("controller2");
-      this.controllersConnected.controller2 = false;
-      hasChanged = true;
-    }
-    //handle that controller 2 has connected
-    else if(!this.controllersConnected.controller2 && this.controller2.getGamepad() !== undefined) {
-      connect.controller2 = this.controller2.getGamepad().id;
-      this.controllersConnected.controller2 = true;
-      hasChanged = true;
-    }
-
-
-    //handle the case that either controller was connected/disconnected
-    if(hasChanged) {
-      if(Array.isArray(disconnect) && disconnect.length) {
-        controllerObj.disconnect = disconnect;
-      }
-      if(Object.keys(connect).length !== 0) {
-        controllerObj.connect = connect;
-      }
-
-      //if status of at least one controller has changed, inform backend
-      if(controllerObj.disconnect || controllerObj.connect) {
-        this.updateQueue.push(controllerObj);
-      }
-    }
+    Sender.sendControllerUpdate.call(this);
   },
 
   updateAndSendPositions() {
@@ -1234,7 +1003,7 @@ export default VRRendering.extend(Ember.Evented, {
    */
   onSelfConnecting(data) {
     //if name is not found, use id as default name
-    let name = this.get('session.data.authenticated.username') ? this.get('session.data.authenticated.username') : data.id;
+    let name = this.get('session.data.authenticated.username') ? this.get('session.data.authenticated.username') : "ID: " + data.id;
     this.set('userID', data.id);
     let JSONObj = {
       "event": "receive_connect_request",
@@ -1307,7 +1076,7 @@ export default VRRendering.extend(Ember.Evented, {
 
     this.addUsername(data.user.id);
 
-    this.enqueueMessage({title: 'User connected', text: user.get('name'), color: this.rgbToHex(user.get('color'))});
+    this.enqueueMessage({title: 'User connected', text: user.get('name'), color: Helper.rgbToHex(user.get('color'))});
 
   },
 
@@ -1585,7 +1354,7 @@ export default VRRendering.extend(Ember.Evented, {
     let camera = user.get('camera').model;
     let username = user.get('name');
 
-    let textSize = this.getTextSize(username);
+    let textSize = Helper.getTextSize(username);
     console.log("Size: " + textSize.width + ", " + textSize.height);
 
     //note: sprites are always same width + height
@@ -1603,7 +1372,7 @@ export default VRRendering.extend(Ember.Evented, {
 
 
     ctx.font = `30px arial`;
-    ctx.fillStyle = this.rgbToHex(user.get('color'));
+    ctx.fillStyle = Helper.rgbToHex(user.get('color'));
     ctx.textAlign = 'center';
     ctx.fillText(username, canvas2.width / 2, 35);
        
@@ -1760,57 +1529,5 @@ export default VRRendering.extend(Ember.Evented, {
     this.deltaTime = 0;
     this.updateQueue = [];
   },
-
-    /**
-   * Performs a binary search on the given array.
-   * 
-   * @param {Array} array The array to search through.
-   * @param {*} searchElement The item to search for within the array.
-   * @return {Number} The index of the element which defaults to -1 when not found.
-   */
-  binaryIndexOf(array, searchElement) {
-    var minIndex = 0;
-    var maxIndex = array.length - 1;
-    var currentIndex;
-    var currentElement;
-
-    while (minIndex <= maxIndex) {
-        currentIndex = (minIndex + maxIndex) / 2 | 0;
-        currentElement = array[currentIndex];
-
-        if (currentElement < searchElement) {
-            minIndex = currentIndex + 1;
-        }
-        else if (currentElement > searchElement) {
-            maxIndex = currentIndex - 1;
-        }
-        else {
-            return currentIndex;
-        }
-    }
-
-    return -1;
-  },
-
-  /**
-   * 
-   * @param {string} text The text to measure the width, height and subline height of.
-   * @param {string} font The font to measure the size in.
-   * @return {{width: Number, height: Number, sublineHeight: Number}} The sizes of the text.
-   */
-  getTextSize(text, font) {
-    // re-use canvas object for better performance
-    let canvas = document.createElement("canvas");
-    let context = canvas.getContext("2d");
-    context.font = font;
-    let width = context.measureText(text).width;
-    let height = context.measureText("W").width;
-    var sublineHeight = context.measureText("H").width;
-    return { width, height, sublineHeight };
-  },
-
-  rgbToHex(rgbArray) {
-    return "#" + ((1 << 24) + (rgbArray[0] << 16) + (rgbArray[1] << 8) + rgbArray[2]).toString(16).slice(1);
-  }
 
 });
