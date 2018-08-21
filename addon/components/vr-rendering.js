d1a65f5d7e653324b7d84a797ec7dbc95d47a54b
mha <stu126317@mail.uni-kiel.de>
2018-08-21 15:29:47 +0200

    fix import

--- addon/components/vr-rendering.js
+++ addon/components/vr-rendering.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-import THREE from "../../../AppData/Local/Microsoft/TypeScript/2.9/node_modules/@types/three";
+import THREE from "three";
 import THREEPerformance from 'explorviz-frontend/mixins/threejs-performance';
 import Raycaster from '../utils/vr-rendering/raycaster';
 import applyKlayLayout from 'explorviz-frontend/utils/landscape-rendering/klay-layouter';
